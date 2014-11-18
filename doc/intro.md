7b0745cc371bb0a7c04cc39f4a68757fb6acb0aa
jtcb <jtcbrule@gmail.com>
2014-11-18 18:46:51 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to pscan
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/pscan/core_test.clj
@@ -0,0 +1,7 @@
+(ns pscan.core-test
+  (:require [clojure.test :refer :all]
+            [pscan.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
