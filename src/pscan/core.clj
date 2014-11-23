9b531e2d567c77cd2ffdaa69f4c1c68617f3d946
jtcb <jtcbrule@gmail.com>
2014-11-23 18:06:54 -0500

    Added region-query-serial

--- src/pscan/core.clj
+++ src/pscan/core.clj
@@ -1,6 +1,41 @@
-(ns pscan.core)
+(ns pscan.core 
+  (:require [clojure.core.reducers :as r]
+            [clojure.set :as s]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def pts [[0 0] [0 1] [0 2] [1 0] [1 1] [1 2] [2 0] [2 1] [2 2]])
+
+(defn r2-dist
+  "Euclidian distance for points in R^2 (for testing)
+   pt is a vector of length 2"
+  [a b]
+  (if (or (nil? a) (nil? b))
+    Double/POSITIVE_INFINITY
+  ;else
+    (let [dx (- (a 0) (b 0))
+          dy (- (a 1) (b 1))]
+      (Math/sqrt (+ (* dx dx) (* dy dy))))))
+
+(defn region-query-serial
+  "Serial region query; returns all pts within eps of pt by metric."
+  [pt pts metric eps]
+  (letfn [(is-local? [x] (<= (metric pt x) eps))]
+    (into [] (filter is-local? pts))))
+
+
+(comment
+(def x (into [] (take 10000000 (repeatedly rand))))
+
+(time (into [] (filter #(> % 0.999999) x)))
+
+(time (into #{} (filter #(> % 0.999999) x)))
+
+(time (r/fold (r/monoid into vector) conj (r/filter #(> % 0.999999) x)))
+
+(time (r/fold (r/monoid into hash-set) conj (r/filter #(> % 0.999999) x)))
+
+(time (r/fold (r/monoid s/union hash-set) conj (r/filter #(> % 0.999999) x)))
+
+(region-query-serial [0 0] pts r2-dist 2)
+  
+  )
+
