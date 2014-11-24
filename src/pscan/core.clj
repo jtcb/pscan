db408c940289c8e2fb4640b1a554ddf331bc1f3d
jtcb <jtcbrule@gmail.com>
2014-11-24 17:21:28 -0500

    Added expand-cluster

--- src/pscan/core.clj
+++ src/pscan/core.clj
@@ -15,14 +15,56 @@
           dy (- (a 1) (b 1))]
       (Math/sqrt (+ (* dx dx) (* dy dy))))))
 
-(defn region-query-serial
+(defn region-query
   "Serial region query; returns all pts within eps of pt by metric."
   [pt pts metric eps]
   (letfn [(is-local? [x] (<= (metric pt x) eps))]
     (into [] (filter is-local? pts))))
 
+(defn expand-cluster
+  "Expand cluster at pt
+   Returns false if there is not cluster to be had
+   Returns the core points of the cluster and an updated visited set otherwise" 
+  [pt pts metric eps min-pts visited]
+
+  (let [local (region-query pt pts metric eps)]
+    (if (< (count local) min-pts)
+      false
+    ;else
+      (loop [s (disj (set (remove #(visited %) local)) pt)
+             c #{pt} 
+             v (conj visited pt)]
+        (if (empty? s)
+          {:core c, :visited v} 
+        ;else 
+          (let [current-point (first s)
+                result (region-query current-point pts metric eps)
+                size (count result)
+                unvisited-result (filter #(not (v %)) result)]
+                
+            (if (>= size min-pts) ;new core point
+              (recur (disj (into s unvisited-result) current-point)
+                     (conj c current-point)
+                     (conj v current-point))
+            ;else
+              (recur (disj s current-point)
+                     c 
+                     (conj v current-point)))))))))
+
+
+
+
 
 (comment
+
+(def star [[0 0] [0 1] [1 0] [-1 0] [0 -1]])
+
+(expand-cluster [0 0] star r2-dist 1 2 #{})
+
+(expand-cluster [0 0] star r2-dist 1 5 #{})
+
+(expand-cluster [-1 0] star r2-dist 1.5 5 #{})
+
 (def x (into [] (take 10000000 (repeatedly rand))))
 
 (time (into [] (filter #(> % 0.999999) x)))
@@ -35,7 +77,8 @@
 
 (time (r/fold (r/monoid s/union hash-set) conj (r/filter #(> % 0.999999) x)))
 
-(region-query-serial [0 0] pts r2-dist 2)
+(region-query [0 0] pts r2-dist 2)
   
-  )
+(map #(region-query % pts r2-dist 1) '([0 0] [0 1]))
+)
 
